1593a2f51ce22f6d0e4b362dbd3cd63d7182b006
Javier Pineda <javierpineda85@gmail.com>
2023-11-17 10:50:21 -0300

    autor y descripcion
    Se modificó el archivo de head.php incorporando los datos de autor y de descripción del sitio

--- vistas/contenido/head.php
+++ vistas/contenido/head.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE </title>
+  <meta name="author" content="Javier Pineda">
+  <meta name="description" content="Sistema de administración de estudiantes y cursos de e-learning realizado en PHP ">
+  <title>e-learning </title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
