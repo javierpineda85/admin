c40b0d03169a4ac911aaa5e8a18390b9b0dc5267
Javier Pineda <javierpineda85@gmail.com>
2023-11-18 20:31:44 -0300

    Correcion de PDO y errores
    Se corrigió la conexión a la base de datos a fin de que pueda mostrar errores y crear rollback en la base de datos en caso de que se produzca un inconveniente

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,10 +1,25 @@
 <?php
-class Conexion{
-    static public function conectar(){
-        //Declaramos los parametros de conexion
-        $link =  new PDO("mysql:host=localhost; port=3306;dbname=classroom","root","");
-        $link->exec("set names utf8"); // esto es para no tener problemas con los caracteres
-        return $link;
+class Conexion {
+    static public function conectar() {
+        try {
+            // Declaramos los parámetros de conexión
+            $link = new PDO("mysql:host=localhost; port=3306;dbname=classroom", "root", "");
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $link->exec("set names utf8"); // Esto es para no tener problemas con los caracteres
+            
+            // Iniciar la transacción
+            $link->beginTransaction();
+            
+            return $link;
+        } catch (PDOException $e) {
+            // En caso de error, realiza un rollback y muestra un mensaje de error
+            if ($link) {
+                $link->rollBack();
+            }
+            echo "Error de conexión: " . $e->getMessage();
+            return null;
+        }
     }
 }
+
 ?>
