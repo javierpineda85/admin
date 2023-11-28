5394b56c0fa24d81c229c4b590818c83e5c0f7bd
Javier Pineda <javierpineda85@gmail.com>
2023-11-28 16:21:59 -0300

    Actualización de script
    Se agregaron funciones de ajax para mejorar el funcionamiento de los formularios del sistema

--- vistas/contenido/scripts.php
+++ vistas/contenido/scripts.php
@@ -34,7 +34,7 @@
 <!-- jQuery -->
 <script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- DataTables  & Plugins -->
 <script src="./plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="./plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
@@ -50,8 +50,14 @@
 <script src="./plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
-<!-- Bootstrap4 Duallistbox -->
-<script src="./plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- Bootstrap4 Duallistbox 
+<script src="./plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>-->
+<script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+
+
+<!-- Bootstrap Switch -->
+<script src="./plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+
 
 <script>
   $(function() {
@@ -71,4 +77,70 @@
       "responsive": true,
     });
   });
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//                             funcion para ocultar secciones
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+  function ocultar() {
+    $e = document.getElementById('ocultar');
+    $e.classList.toggle('d-none');
+  }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//       no permite enviar el formulario de editar-perfil sin que no se presione submit
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+  $("#editar-perfil").submit(function (event) {
+    var fnac = $('#fnac').val();
+    var domicilioPerfil = $('#domicilioPerfil').val();
+    var errores = [];
+
+    if ($.trim(fnac) === '') {
+        errores.push("Debe completar la fecha de nacimiento");
+    }
+    if ($.trim(domicilioPerfil) === '') {
+        errores.push("Debe completar el domiclio");
+    }
+
+
+    if (errores.length > 0) {
+        // Muestra los errores como toasts en lugar de un alert
+        Toastify({
+            text: errores.join(" / "),
+            duration: 3000, // Duración en milisegundos
+            close: true, // Agregar un botón para cerrar el toast
+            gravity: "top", // Posición del toast (puedes ajustarla según tus preferencias)
+        }).showToast();
+
+        event.preventDefault(); // Evita que el formulario se envíe
+    }
+    
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//funcion para completar el DNI y contraseña al mismo tiempo en  paginas/usuarios/crear-usuario.php
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function() {
+      $('#inputDNI').on('input', function() {
+        var valor = $(this).val();
+        var input2 = $('#inputPass');
+        var caracteresRestantes = $('#caracteresRestantes');
+
+        // Limitar la longitud a 8 caracteres
+        if (valor.length > 8) {
+          valor = valor.slice(0, 8);
+        }
+
+        // Actualizar el valor del segundo input
+        input2.val(valor);
+
+        // Calcular y mostrar los caracteres restantes
+        var restantes = 8 - valor.length;
+        caracteresRestantes.text('Caracteres restantes: ' + restantes);
+      });
+    });
+
+
 </script>
