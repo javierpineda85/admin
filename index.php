8725620363f3a00e7ace89fe1ead09fca007adcb
Javier Pineda <javierpineda85@gmail.com>
2024-04-28 20:38:14 -0300

    Actualización de index
    Se actualizaron los controladores

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
+require_once("config.php");
+require_once("controladores/rutas.controller.php");
 require_once("controladores/plantilla.controller.php");
-//require_once ("controladores/formularios.controller.php");
-//require_once ("modelos/formularios.modelo.php");
+require_once("controladores/usuarios.controller.php");
+require_once("controladores/cursos.controller.php");
+require_once("controladores/materias.controller.php");
+require_once("controladores/perfiles.controller.php");
+require_once("controladores/mensajes.controller.php");
 
-//Instanciar objeto
 
 $plantilla= new PlantillaController();
  // ejecutar metodo
 $plantilla->crtGetPlantilla();
+
+
 ?>
