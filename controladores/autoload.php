cb6b80501f794752974fa753ad6c734db3f5f9ea
Javier Pineda <javierpineda85@gmail.com>
2024-05-02 10:52:27 -0300

    Correcion de archivo autoload.php
    Se modificó el código para su optimización

--- controladores/autoload.php
+++ controladores/autoload.php
@@ -1,33 +1,25 @@
 <?php
 
-class autocarga {
-
-static public function cargarClases($clase)
+class autocarga
 {
-    
-    $clases= array();
-    $clases['cursos']= "controladores/cursos.controller.php";
-    $clases['materias']="controladores/materias.controller.php";
-    $clases['mensajes']="controladores/mensajes.controller.php";
-    $clases['perfiles']="controladores/perfiles.controller.php";
-    $clases['usuarios']="controladores/usuarios.controller.php";    
-    $clases['plantilla']="controladores/plantilla.controller.php";
-    
-   
+    static public function cargarClases($clase)
+    {
+        $clases = array(
+            'cursos'    => "controladores/cursos.controller.php",
+            'materias'  => "controladores/materias.controller.php",
+            'mensajes'  => "controladores/mensajes.controller.php",
+            'perfiles'  => "controladores/perfiles.controller.php",
+            'usuarios'  => "controladores/usuarios.controller.php",
+            'plantilla' => "controladores/plantilla.controller.php",
+            'rutas'     => "controladores/rutas.controller.php"
+        );
 
-    if (isset($clases[$clase])) {
-
-        if (file_exists($clases[$clase])) {
-            include $clases[$clase];
-
-        } else {
-            throw new Exception("Error al cargar la clase" . $clases[$clase]);
-            
+        if (isset($clases[$clase])) {
+            if (file_exists($clases[$clase])) {
+                include $clases[$clase];
+            } else {
+                throw new Exception("Error al cargar la clase" . $clases[$clase]);
+            }
         }
-
     }
-      
 }
-}
-spl_autoload_register('autocarga::cargarClases');
-?>
