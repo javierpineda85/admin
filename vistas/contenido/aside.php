0b253afe1bdf1ea330e24ac45baf4c48024baaf1
Javier Pineda <javierpineda85@gmail.com>
2026-06-01 18:44:11 -0300

    Save latest branding and cleanup changes

--- vistas/contenido/aside.php
+++ vistas/contenido/aside.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
       <img src="img/logoCampus.png" alt="Logo Campus MenteMotion" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Campus MenteMotion</span>
+      <span class="brand-text font-weight-light">Campus</span>
     </a>
 
     <!-- Sidebar -->
