66430e4ad9a498031d6bb64ed0efa46fac21d65f
Javier Pineda <javierpineda85@gmail.com>
2026-06-01 16:27:31 -0300

    Update footer branding

--- vistas/contenido/footer.php
+++ vistas/contenido/footer.php
@@ -1,4 +1,4 @@
 <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 3.1.0626
     </div>
-    <strong>Copyright &copy; 2024. Desarrollado por <a href="https://thebigtable.com.ar"> The Big Table,</a> con plantillas de AdminLTE.io.</strong>
+    <strong>Copyright &copy; 2026. Desarrollado por <a href="https://thebigtable.com.ar"> The Big Table,</a> potenciado con IA</strong>
